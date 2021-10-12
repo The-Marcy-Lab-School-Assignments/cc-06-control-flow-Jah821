88c0042f9482b8d6582d199a2bb183ebcfcab53b
Jah821 <91166820+Jah821@users.noreply.github.com>
2021-10-12 14:24:28 +0000

    code challange 6

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 //1
 function sumOfThreeAndFive(){
-  
+  let sum = 0
+  for (let i = 0; i < 1000; i++){
+      if (i % 3 === 0 && i % 5 === 0){
+          sum += i
+      }
+  } 
+  return sum
 }
 
+console.log(sumOfThreeAndFive())
 function greaterNum(){
-  
+  if (num === num2){;
+      return"both integers are equal"
+  }else if(num>num2){
+      return num
+  }
 }
 
 function oddAndEvenInFifteen(){
